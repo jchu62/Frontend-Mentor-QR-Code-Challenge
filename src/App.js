129308a46e60dc415effebfc5c9ee37329ac4c7c
jchu62 <89236068+jchu62@users.noreply.github.com>
2024-01-19 23:01:15 -0800

    Center bar code and description

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
+import qrcode from './image-qr-code.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
+        <header className="box">
+        <img src={qrcode} className="logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Improve your front-end skills by building projects
+        </p>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        </header>
       </header>
     </div>
   );
